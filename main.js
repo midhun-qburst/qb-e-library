d73f8ab73a4f848dc4a3163a4fc94464c24acd3f
mindphreaked <midhuns@qburst.com>
2017-11-15 13:31:56 +0530

    Custom validations done. added styling to invalid mandatory fields.

--- main.js
+++ main.js
@@ -1,50 +1,76 @@
-let saveBook = () => {
-    const bookTitle = document.getElementById('title').value;
-    const bookAuthor = document.getElementById('author').value;
-    const bookPrice = document.getElementById('price').value;
-    const bookSummary = document.getElementById('summary').value;
-    const mail = document.getElementById('authorMail').value;
-    const phone = document.getElementById('authorPhone').value;
+let saveBook = (event) => {
+  const bookTitle = document.getElementById('title').value;
+  const bookAuthor = document.getElementById('author').value;
+  const bookPrice = document.getElementById('price').value;
+  const bookSummary = document.getElementById('summary').value;
+  const mail = document.getElementById('authorMail').value;
+  const phone = document.getElementById('authorPhone').value;
+  const emailPattern = '(.+)@(.+){2,}\.(.+){2,}';
+  const emailRegex = RegExp(emailPattern);
+  const phonePattern = '[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}';
+  const phoneRegex = RegExp(phonePattern);
+  const pricePattern = '[0-9]';
+  const priceRegex = RegExp(pricePattern);
+  if (bookTitle.length == 0) {
+    alert("Hey.. You forgot to name me..!");
+    event.preventDefault();
+    }
+  if (bookAuthor.length == 0) {
+    alert("Who created me ?");
+    event.preventDefault();  
+  }
+  if(!priceRegex.test(bookPrice) && bookPrice.length > 0) {
+    alert("Please Enter the numeric price..!");
+    event.preventDefault();
+  }
+  if(!emailRegex.test(mail) && mail.length > 0) {
+    alert("Please enter a valid email address..!");
+    event.preventDefault();
+  }
+  if(!phoneRegex.test(phone) && phone.length > 0) {
+    alert("Please enter a valid phone number..!");
+    event.preventDefault();
+  }
+  
+  else {
     var book = {
-        title: bookTitle,
-        author: bookAuthor,
-        price: bookPrice,
-        summary: bookSummary,
-        authorMail: mail,
-        authorPhone: phone
+      title: bookTitle,
+      author: bookAuthor,
+      price: bookPrice,
+      summary: bookSummary,
+      authorMail: mail,
+      authorPhone: phone
     }
 
-    if(localStorage.getItem('books') === null)
-    {
-     let books = [];
-     books.push(book);
-     localStorage.setItem('books', JSON.stringify(books));
-   } else {
-     let books = JSON.parse(localStorage.getItem('books'));
-     books.push(book);
-     localStorage.setItem('books', JSON.stringify(books));
-   }
-     
+    if (localStorage.getItem('books') === null) {
+      let books = [];
+      books.push(book);
+      localStorage.setItem('books', JSON.stringify(books));
+    } else {
+      let books = JSON.parse(localStorage.getItem('books'));
+      books.push(book);
+      localStorage.setItem('books', JSON.stringify(books));
+    }
+  }
 }
 
-let getBookList = (event)=> {
+let getBookList = (event) => {
   const books = JSON.parse(localStorage.getItem('books'));
   let bookList = document.getElementById('bookList');
   bookList.innerHTML = '';
 
   books.forEach(element => {
     console.log(element);
-    const {title, author, price, summary, authorMail, authorPhone} = element;
+    const { title, author, price, summary, authorMail, authorPhone } = element;
 
-    $('#bookList').append(  '<div >' + '<span>' + title + '</span>' +
-    '<span>' + author + '</span>' +
-    //'<div>' + price + '</div>' +
-    //'<div>' + summary + '</div>' +
-    //'<div>' + authormail + '</div>' +
-    //'<div>' + authorPhone + '</div>' +
-    '</div>'
-  ); 
+    $('#bookList').append('<div >' + '<span>' + title + '</span>' +
+      '<span>' + author + '</span>' +
+      //'<span>' + price + '</span>' +
+      //'<div>' + summary + '</span>' +
+      //'<div>' + authormail + '</span>' +
+      //'<span>' + authorPhone + '</span>' +
+      '</div>'
+    );
     event.preventDefault();
   });
-
 }
