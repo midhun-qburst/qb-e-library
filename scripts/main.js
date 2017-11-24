e95e6b31854515dabb6aad60c7ae9619ffb6e7b4
mindphreaked <midhuns@qburst.com>
2017-11-24 15:18:10 +0530

    Add modal created.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,29 @@
+// Get the modal
+const modal = document.getElementById('addBookModal');
+
+// Get the button that opens the modal
+const btn = document.getElementById("addBookBtn");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = () => {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = () => {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = (event) => {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 /**
  * Saves details of a book to local storage.
  * @param {*} event 
